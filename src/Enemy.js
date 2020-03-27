68437686259e93313779c278c6761dc11fce7c02
KostraOne <guillaume.amangoua@gmail.com>
2020-03-27 13:32:24 +0100

    initial commit

---
+++ src/Enemy.js
@@ -0,0 +1,24 @@
+/*
+
+conseil:
+la majorité des fonctions de cette classe peuvent être codées en 1 ou 2 ligne(s)
+
+
+helpers:
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes/constructor
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/return
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions/Valeurs_par_d%C3%A9faut_des_arguments
+
+*/
+
+Enemy = null;
+Skull = null;
+Orc = null;
+
+module.exports = {
+  Enemy,
+  Skull,
+  Orc
+};
---
+++ src/Player.js
@@ -0,0 +1,16 @@
+/*
+
+conseil:
+- la majorité des fonctions de cette classe peuvent être codées en 1 ou 2 ligne(s)
+- rappel : D.R.Y !
+
+helpers:
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes/constructor
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/return
+- https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions/Valeurs_par_d%C3%A9faut_des_arguments
+
+*/
+
+const Player = null;
+module.exports = Player
